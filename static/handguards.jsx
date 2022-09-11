28bd2cf0e2acb4287a6442486930203831c8e364
Robert <osipovitchr@gmail.com>
2022-09-11 11:09:48 -0400

    Handguards page static content

---
+++ static/handguards.jsx
@@ -0,0 +1,88 @@
+export const handguardsPage = {
+
+    // Hero Image Section
+    heroImageSection: {
+        companyName: "Isler Custom Gun Works",
+        imageText: [
+            "Lightweight Carbon Fiber Handguards",
+            "And",
+            "Accessories"
+        ],
+        imagePath: "/images/handguards-hero-image.jpg",
+    },
+
+    // Text Content Section
+    textContentSection: {
+        title: "What we can do for you!",
+        text: [
+            "When we install our handguards for our clientele we routinely remove the mil spec A2 front sights and install an SLR Steel or Titanium Adjustable Gas Block or whatever gas block you desire. Barrel removal and installation of upgraded barrels and upgraded muzzle brake installation are routinely performed. And heck, it doesn't have to be our handguards, it can be whatever make/look you desire!",
+            "Our Custom Shop can also install your handguard choice to your upper receiver and make the modifications you desire or ask for our input depending on your application.",
+        ]
+    },
+
+    // Handguards section
+    handguardsSection: {
+        handguards: [
+            {
+                title: "AR15/AR9 COMPETITION CARBON FIBER HANDGUARD FOR 3 GUN AND PCC COMPETITORS",
+                imagePath: "/images/handguards/competition-carbon-fiber-handguard.jpg",
+                p1: "The Competition Model is designed to employ any number of M-Lok style attachments. M-Lok design is machined into the handguard every 45 degrees for the entire length of the handguard assuring that any M-Lok hand stop or barrier stop can be located where you desire. The M-Lok design aslo offers great grip. This design utilizes a 7075 Aluminum barrel nut that incorporates the handguard mount. It is lightened to minimal weight (75 gr) and still maintains strength and superior handguard stiffness. The barrel nut/mount is hardcoated with tapping that is form thread tapped for greater strength. A shim kit is supplied to phase the barrel nut to the proper location for mounting the handguard. A proprietary barrel nut wrench is also supplied for installing and removing the barrel nut/mount.",
+                p2: `The handguard is 2.000" O.D. and 1.840'' I.D. The .080" wall thickness and gives extremely good stiffness at any length. The fasteners are supplied and are quality Torx Plus button head design. The Torx Plus wrenches are also included in the kit.`,
+                weights: [
+                    `17” Competition -- 8.3 ounces`,
+                    `15” Competition -- 7.7 ounces`,
+                    `13.5” Competition -- 7.2 ounces`
+                ],
+            },
+            {
+                title: "THE AR10 LR308 DPMS Platform Handguard",
+                imagePath: "/images/handguards/ar10-lr308-dpms-platform-handguard.jpg",
+                p1: `The same design as our AR15/AR9 Competition Handguard . A slightly larger outer diameter with 100% M-Lok slots. 2.160" O.D. with a 2.000" I.D. Bull Barrels with SLR or Superlative Adjustable gas blocks clear handguard with no issue! These will initially be offered in 17", 15" and 13.5". We project the 17" handguard installed to be 10 oz!`,
+                weights: [
+                    "17” Competition -- 10.1 ounces",
+                    "15” Competition -- 9.3 ounces",
+                    "13.5” Competition -- 8.5 ounces",
+                ],
+            },
+            {
+                title: "COMPETITION CARBON FIBER HANDGUARD FOR MPX",
+                imagePath: "/images/handguards/competition-carbon-fiber-handguard-for-mpx.jpg",
+                p1: `The 13.5" MPX carbon fiber handguard, like our other handguards, are made from Aerospace quality carbon fiber. The handguard employs the MPX retention pin for mounting the lower portion of the handguard to the front take down pin. A left and right CNC machined 7075 hardcoated aluminum rail attaches to the carbon fiber tube with fasteners to give a solid mount feel when the handguard is attached to the upper receiver.`,
+                p2: `This hanguard has M-Lok every 45 Degrees for a positive grip and for your finger and barrier stops. The tube size is 2.000"OD x 1.840"ID. Also the round tube has a more natural feel than the production and competitors part.`,
+                weights: [
+                    `6” Competition -- XXX grams/ XXX ounces`,
+                    `8” Competition -- 3.6 ounces`,
+                    `10.25” Competition -- 4.0 ounces`,
+                    `12.25” Competition -- 4.7 ounces`,
+                    `13.5” Competition -- 5.1 ounces`,
+                    `15” Competition -- 5.5 ounces`,
+
+                ],
+            },
+            {
+                title: "AR-9 COMPETITION HANDGUARD 1 3/4’ DIAMETER",
+                imagePath: "/images/handguards/ar-9-competition-handguard-1-3-4-diameter.jpg",
+                p1: `We have 15”, 13.5” and 12.75" available. Other lengths will be available in the future. The 15" is 6.4 oz complete, installed. M-Lok every 90 degrees and smaller lightening slots on the 45’s on this .060” wall aerospace quality filament wound carbon fiber tube. The M-Lok and lightening slots provide fresh grabbing grip and allow the ability to add finger stops, laser mounting and other desired accessories. The Barrel Nut is lightweight 7075 Hardcoated Aluminum.`,
+                p2: `This handguard is mounted with 12 super torx button head screws to the barrel nut for unbelievable stiffness. The kit comes with barrel nut shims to phase the barrel nut for correct tube mounting, proprietary barrel nut wrench, torx plus screws and a TP15 bit for tightening the screws.`,
+                weights: [
+                    `15” Competition -- 6.4 ounces`,
+                    `13.5” Competition -- 6.0 ounces`,
+                    `12.75” Competition -- 5.8 ounces`,
+                ],
+            },
+            {
+                title: "MPX PIC RAIL SECTION",
+                imagePath: "/images/handguards/mpx-pic-rail-section.jpg",
+                p1: `Available now, our new MPX Picatinny Rail section specifically designed to work with our MPX Carbon Fiber Handguards. This rail matches the height of the rail on the MPX upper receiver so you can mount your back-up or transition sights.`,
+                p2: `The Pic Rail incorporates a proprietary mount plate designed to work with the M-Lok mount system used in all of our Competition Handguards. The rail and mount plate are made from 7075 Aluminum and are finished in black anodize. It is 1.750" long and the total weight is a mere 18 grams. A Torx Plus TP8 L key is supplied to tighten fasteners.`,
+                p3: `NOTE: For those of you who prefer a laser on your MPX handguards use our AR15/AR9 handguard Pic Rail. The short profile will keep the laser housing out of your optic view due to its low profile. Total weight is 10 grams.`,
+            },
+            {
+                title: "AR15/AR9 MPX PIC RAIL SECTION",
+                imagePath: "/images/handguards/mpx-pic-rail-section.jpg",
+                p1: `Avaiable now, our new AR15/AR9 Picatinny Rail section specifically designed to work with our AR-15/AR9 Competition Handguards. This rail matches the height of the AR upper receiver so that you can mount your backup or transition sights. The Pic Rail incorporates a proprietary mount plate designed to work with the M-Lok mount system used in all of our Competition Handguards. The rail and mount plate are made from 7075 Aluminum and are finished in Black anodize. It is 1.750" long and the total weight is 10 grams. A Torx Plus TP8 L key wrench is supplied to tighten fasteners.`,
+            }
+        ]
+    },
+
+}
